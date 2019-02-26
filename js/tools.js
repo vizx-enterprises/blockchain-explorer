0751d608945291cde8118bd6d75049b4970875af
Brandon Lehmann <brandonlehmann@gmail.com>
2019-02-26 15:53:47 -0500

    Adding generate new wallet functions to the tools page & formatting resulting files

--- js/tools.js
+++ js/tools.js
@@ -3,32 +3,89 @@ $(document).ready(function () {
     decodeAddress()
   })
 
+  $('#generateWalletButton').click(function () {
+    generateWallet(true)
+  })
+
   $('#walletAddress').keydown(function (e) {
     if (e.which === 13) {
       decodeAddress()
     }
   })
 
-  $('#publicViewKey').keydown(function () {
-    encodeAddress()
+  $('#restoreFromSeedButton').click(function () {
+    $('#mnemonic').val('')
+    generateWallet(false)
   })
 
-  $('#publicSpendKey').keydown(function () {
-    encodeAddress()
+  $('#restoreFromMnemonicButton').click(function () {
+    $('#seed').val('')
+    generateWallet(false)
   })
 
-  $('#paymentId').keydown(function () {
+  $('#encodeAddressButton').click(function () {
     encodeAddress()
   })
 
   window.cnUtils = new TurtleCoinUtils.CryptoNote()
 })
 
+function generateWallet(newWallet) {
+  const entropy = $('#entropy').val().trim()
+  if (newWallet) {
+    $('#seed').val('')
+    $('#mnemonic').val('')
+  }
+  const seed = $('#seed').val().trim()
+  const mnemonic = $('#mnemonic').val().trim().toLowerCase()
+
+  $('#newPrivateSpendKey').val('')
+  $('#newPublicSpendKey').val('')
+  $('#newPrivateViewKey').val('')
+  $('#newPublicViewKey').val('')
+  $('#newWalletAddress').val('')
+
+  $('#entropy').removeClass('is-danger')
+  $('#seed').removeClass('is-danger')
+  $('#mnemonic').removeClass('is-danger')
+
+  var wallet
+
+  if (entropy.length !== 0 && newWallet) {
+    wallet = cnUtils.createNewAddress(entropy)
+  } else if (seed.length !== 0) {
+    if (!isHash(seed)) {
+      return $('#seed').addClass('is-danger')
+    }
+    wallet = cnUtils.createAddressFromSeed(seed)
+  } else if (mnemonic.length !== 0) {
+    if ((mnemonic.split(' ')).length !== 25) {
+      return $('#mnemonic').addClass('is-danger')
+    }
+    wallet = cnUtils.createAddressFromMnemonic(mnemonic)
+  } else if (newWallet) {
+    wallet = cnUtils.createNewAddress()
+  }
+
+  if (!wallet) return
+
+  $('#seed').val(wallet.seed)
+  $('#mnemonic').val(wallet.mnemonic)
+  $('#newPrivateSpendKey').val(wallet.spend.privateKey)
+  $('#newPublicSpendKey').val(wallet.spend.publicKey)
+  $('#newPrivateViewKey').val(wallet.view.privateKey)
+  $('#newPublicViewKey').val(wallet.view.publicKey)
+  $('#newWalletAddress').val(wallet.address)
+
+  return true
+}
+
 function decodeAddress() {
-  var walletAddress = $('#walletAddress').val()
+  const walletAddress = $('#walletAddress').val()
 
   try {
-    var addr = cnUtils.decodeAddress(walletAddress)
+    const addr = cnUtils.decodeAddress(walletAddress)
+
     $('#publicViewKey').val(addr.publicViewKey)
     $('#publicSpendKey').val(addr.publicSpendKey)
     $('#paymentId').val(addr.paymentId)
@@ -40,21 +97,21 @@ function decodeAddress() {
 }
 
 function encodeAddress() {
-  var publicViewKey = $('#publicViewKey').val().trim()
-  var publicSpendKey = $('#publicSpendKey').val().trim()
-  var paymentId = $('#paymentId').val().trim()
+  const publicViewKey = $('#publicViewKey').val().trim()
+  const publicSpendKey = $('#publicSpendKey').val().trim()
+  const paymentId = $('#paymentId').val().trim()
 
   $('#publicViewKey').removeClass('is-danger')
   $('#publicSpendKey').removeClass('is-danger')
   $('#paymentId').removeClass('is-danger')
   $('#encodedAddress').removeClass('is-danger')
 
-  if (!isHash(publicViewKey)) {
-    return $('#publicViewKey').addClass('is-danger')
-  }
-
   if (!isHash(publicSpendKey)) {
     return $('#publicSpendKey').addClass('is-danger')
+  }
+
+  if (!isHash(publicViewKey)) {
+    return $('#publicViewKey').addClass('is-danger')
   }
 
   if (paymentId.length !== 0 && !isHash(paymentId)) {
@@ -62,7 +119,7 @@ function encodeAddress() {
   }
 
   try {
-    var addr = cnUtils.encodeAddress(publicViewKey, publicSpendKey, paymentId)
+    const addr = cnUtils.encodeAddress(publicViewKey, publicSpendKey, paymentId)
     $('#encodedAddress').val(addr)
   } catch (e) {
     $('#encodedAddress').removeClass('is-danger').addClass('is-danger')
