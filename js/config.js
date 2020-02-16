d34f572918f3ca4b01a63a09ad5bae25845f15cf
wrkzdev <40448869+wrkzdev@users.noreply.github.com>
2020-02-17 06:45:06 +0700

    Commit

--- js/config.js
+++ js/config.js
@@ -1,21 +1,21 @@
 const ExplorerConfig = {
-  apiBaseUrl: 'https://dego.bot.tips',
-  ticker: 'DEGO',
-  maxSupply: 10000000000000000,
-  emissionSpeed: 24,
-  decimalPoints: 2,
-  blockTargetTime: 20,
+  apiBaseUrl: 'https://ple.bot.tips',
+  ticker: 'PLE',
+  maxSupply: 2100000000000000000,
+  emissionSpeed: 22,
+  decimalPoints: 8,
+  blockTargetTime: 120,
   startYear: 2018,
-  genesisTimestamp: 1545261161,
+  genesisTimestamp: 1533098345,
   emissionCurveInterval: 2000,
   emissionCurveDataPoints: 30000,
-  addressPrefix: 8411,
+  addressPrefix: 18181,
   forkHeights: [
-    700000,
-    1470000,
-    1980000,
-    2250000,
-    2325000,
-    2400000
+    65500,
+    67500,
+    130000,
+    280000,
+    400000,
+    500000
   ]
 }
